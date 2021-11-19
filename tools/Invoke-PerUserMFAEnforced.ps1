9023c529a0a6243283827d9d39b508b584d3519a
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2021-11-19 15:38:26 -0600

    k

---
+++ tools/Invoke-PerUserMFAEnforced.ps1
@@ -0,0 +1,25 @@
+#https://www.alitajran.com/enable-mfa-office-365-powershell/
+
+<#https://docs.microsoft.com/en-us/azure/active-directory/roles/permissions-reference#authentication-administrator
+One of the following roles are required
+Role	                                  | Manage user's auth methods	         | Manage per-user MFA	           | Manage MFA settings | Manage auth method policy | Manage password protection policy
+Authentication Administrator             | Yes for some users (see above)       | Yes for some users (see above)  |	No	                |  No	                    | No
+Privileged Authentication Administrator	| Yes for all users	                   |Yes for all users                |No	               | No	                       | No
+Authentication Policy Administrator    | No                                   |	No                              | Yes                 | Yes	                      | Yes
+#>
+
+
+
+
+function Invoke-PerUserMFAEnforced {
+    $upns = get-msoluser | Out-GridView -PassThru -Title "Select the users who you are Enforcing MFA for."
+
+    $mf = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+    $mf.RelyingParty = "*"
+    $mf.State = "Enforced"
+    $mfa = @($mf)
+
+    foreach ($upn in $upns) {
+        Set-MsolUser -UserPrincipalName $upn.UserPrincipalName -StrongAuthenticationRequirements $mfa
+    }
+}
