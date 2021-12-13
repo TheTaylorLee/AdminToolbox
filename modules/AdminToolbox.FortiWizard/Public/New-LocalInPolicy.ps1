bf930286e9221c4e692a168b22756be0ef374681
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2021-12-12 18:07:20 -0600

    Update get-fortiwizard

--- modules/AdminToolbox.FortiWizard/Public/New-LocalInPolicy.ps1
+++ modules/AdminToolbox.FortiWizard/Public/New-LocalInPolicy.ps1
@@ -1,3 +1,4 @@
+
 # Figure logic then write function
 
 # New-LocalInPolicyTrusthosts Private function is called to set trusthosts for listed admins. Max integer loops are used to count up for all trusthosts and apply for all admins comma delimited. figure out a way to handle the variable amount of trust hosts that can be added.
