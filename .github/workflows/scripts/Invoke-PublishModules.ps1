892d09543500f46f1ccd9077398df409acc1b4d8
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-07-11 09:56:04 -0500

    typo fix

--- .github/workflows/scripts/Invoke-PublishModules.ps1
+++ .github/workflows/scripts/Invoke-PublishModules.ps1
@@ -7,7 +7,7 @@ function Invoke-PublishModules {
     if ([version]$ActiveDirectoryGithub -gt [version]$ActiveDirectoryPSGallery ) {
         New-Manifest -ActiveDirectory #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.ActiveDirectory" -NuGetApiKey $env:NUGET_KEY
-        $ActiveDirectory = Write-Output "[+] AdminToolbox.ActiveDirectory published to PSGalley"
+        $ActiveDirectory = Write-Output "[+] AdminToolbox.ActiveDirectory published to PSGallery"
     }
     else {
         $ActiveDirectory = Write-Output "[-] AdminToolbox.ActiveDirectory not published"
@@ -19,7 +19,7 @@ function Invoke-PublishModules {
     if ([version]$EndpointManagementGithub -gt [version]$EndpointManagementPSGallery ) {
         New-Manifest -EndpointManagement #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.EndpointManagement" -NuGetApiKey $env:NUGET_KEY
-        $EndpointManagement = Write-Output "[+] AdminToolbox.EndpointManagement published to PSGalley"
+        $EndpointManagement = Write-Output "[+] AdminToolbox.EndpointManagement published to PSGallery"
     }
     else {
         $EndpointManagement = Write-Output "[-] AdminToolbox.EndpointManagement not published"
@@ -31,7 +31,7 @@ function Invoke-PublishModules {
     if ([version]$ExchangeGithub -gt [version]$ExchangePSGallery ) {
         New-Manifest -Exchange #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.Exchange" -NuGetApiKey $env:NUGET_KEY
-        $Exchange = Write-Output "[+] AdminToolbox.Exchange published to PSGalley"
+        $Exchange = Write-Output "[+] AdminToolbox.Exchange published to PSGallery"
     }
     else {
         $Exchange = Write-Output "[-] AdminToolbox.Exchange not published"
@@ -43,7 +43,7 @@ function Invoke-PublishModules {
     if ([version]$FFToolsGithub -gt [version]$FFToolsPSGallery ) {
         New-Manifest -FFTools #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.FFTools" -NuGetApiKey $env:NUGET_KEY
-        $FFTools = Write-Output "[+] AdminToolbox.FFTools published to PSGalley"
+        $FFTools = Write-Output "[+] AdminToolbox.FFTools published to PSGallery"
     }
     else {
         $FFTools = Write-Output "[-] AdminToolbox.FFTools not published"
@@ -55,7 +55,7 @@ function Invoke-PublishModules {
     if ([version]$FileManagementGithub -gt [version]$FileManagementPSGallery ) {
         New-Manifest -FileManagement #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.FileManagement" -NuGetApiKey $env:NUGET_KEY
-        $FileManagement = Write-Output "[+] AdminToolbox.FileManagement published to PSGalley"
+        $FileManagement = Write-Output "[+] AdminToolbox.FileManagement published to PSGallery"
     }
     else {
         $FileManagement = Write-Output "[-] AdminToolbox.FileManagement not published"
@@ -67,7 +67,7 @@ function Invoke-PublishModules {
     if ([version]$FunGithub -gt [version]$FunPSGallery ) {
         New-Manifest -Fun #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.Fun" -NuGetApiKey $env:NUGET_KEY
-        $Fun = Write-Output "[+] AdminToolbox.Fun published to PSGalley"
+        $Fun = Write-Output "[+] AdminToolbox.Fun published to PSGallery"
     }
     else {
         $Fun = Write-Output "[-] AdminToolbox.Fun not published"
@@ -79,7 +79,7 @@ function Invoke-PublishModules {
     if ([version]$NetworkingGithub -gt [version]$NetworkingPSGallery ) {
         New-Manifest -Networking #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.Networking" -NuGetApiKey $env:NUGET_KEY
-        $Networking = Write-Output "[+] AdminToolbox.Networking published to PSGalley"
+        $Networking = Write-Output "[+] AdminToolbox.Networking published to PSGallery"
     }
     else {
         $Networking = Write-Output "[-] AdminToolbox.Networking not published"
@@ -91,7 +91,7 @@ function Invoke-PublishModules {
     if ([version]$Office365Github -gt [version]$Office365PSGallery ) {
         New-Manifest -Office365 #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.Office365" -NuGetApiKey $env:NUGET_KEY
-        $Office365 = Write-Output "[+] AdminToolbox.Office365 published to PSGalley"
+        $Office365 = Write-Output "[+] AdminToolbox.Office365 published to PSGallery"
     }
     else {
         $Office365 = Write-Output "[-] AdminToolbox.Office365 not published"
@@ -103,7 +103,7 @@ function Invoke-PublishModules {
     if ([version]$RemotingGithub -gt [version]$RemotingPSGallery ) {
         New-Manifest -Remoting #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.Remoting" -NuGetApiKey $env:NUGET_KEY
-        $Remoting = Write-Output "[+] AdminToolbox.Remoting published to PSGalley"
+        $Remoting = Write-Output "[+] AdminToolbox.Remoting published to PSGallery"
     }
     else {
         $Remoting = Write-Output "[-] AdminToolbox.Remoting not published"
@@ -115,7 +115,7 @@ function Invoke-PublishModules {
     if ([version]$VMWareAutomateGithub -gt [version]$VMWareAutomatePSGallery ) {
         New-Manifest -VMWareAutomate #Generate each modules manifest files
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.VMWareAutomate" -NuGetApiKey $env:NUGET_KEY
-        $VMWareAutomate = Write-Output "[+] AdminToolbox.VMWareAutomate published to PSGalley"
+        $VMWareAutomate = Write-Output "[+] AdminToolbox.VMWareAutomate published to PSGallery"
     }
     else {
         $VMWareAutomate = Write-Output "[-] AdminToolbox.VMWareAutomate not published"
@@ -128,7 +128,7 @@ function Invoke-PublishModules {
         New-Manifest -FortiWizard #Generate each modules manifest files
         Install-Module AdminToolbox.Networking -AllowClobber -Force; Import-Module AdminToolbox.Networking -Force -Global
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox.FortiWizard" -NuGetApiKey $env:NUGET_KEY
-        $FortiWizard = Write-Output "[+] Admintoolbox.FortiWizard published to PSGalley"
+        $FortiWizard = Write-Output "[+] Admintoolbox.FortiWizard published to PSGallery"
     }
     else {
         $FortiWizard = Write-Output "[-] Admintoolbox.FortiWizard not published"
@@ -151,7 +151,7 @@ function Invoke-PublishModules {
         Install-Module AdminToolbox.Remoting -AllowClobber -Force; Import-Module AdminToolbox.Remoting -Force -Global
         Install-Module AdminToolbox.VMWareAutomate -AllowClobber -Force; Import-Module AdminToolbox.VMWareAutomate -Force -Global
         Publish-Module -Path "$workingdirectory/modules/AdminToolbox" -NuGetApiKey $env:NUGET_KEY
-        $AdminToolbox = Write-Output "[+] AdminToolbox published to PSGalley"
+        $AdminToolbox = Write-Output "[+] AdminToolbox published to PSGallery"
     }
     else {
         $AdminToolbox = Write-Output "[-] AdminToolbox not published"
