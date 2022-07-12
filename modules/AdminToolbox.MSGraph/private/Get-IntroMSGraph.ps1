578aeeccf549dbcf3a407bab3987409215f2f988
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-07-12 17:46:31 -0500

    func: get-intromsgraph

---
+++ modules/AdminToolbox.MSGraph/private/Get-IntroMSGraph.ps1
@@ -0,0 +1,30 @@
+Function Get-IntroMSGraph {
+    Write-Host "
+                              __ ___ _______
+                            |   Y   |   _   |
+                            |.      |   1___|
+                            |. \_/  |____   |
+                            |:  |   |:  1   |
+                            |::.|:. |::.. . |
+                            '--- ---'-------'
+                                   _______                  __
+                                  |   _   .----.---.-.-----|  |--.
+                                  |.  |___|   _|  _  |  _  |     |
+                                  |.  |   |__| |___._|   __|__|__|
+                                  |:  1   |          |__|
+                                  |::.. . |
+                                  '-------'
+" -ForegroundColor Green
+
+    Write-Host "             --------------------------------------------------------------------"      -ForegroundColor Green
+    Write-Host "             > "                                                                        -ForegroundColor Green   -NoNewline
+    Write-Host "Admintoolbox.MSGraph "                                                                  -ForegroundColor Red     -NoNewline
+    Write-Host "is the successor to the Office 365 module"                                              -ForegroundColor Yellow     -NoNewline
+    Write-Host "   <"                                                                                   -ForegroundColor Green
+    Write-Host "             > "                                                                        -ForegroundColor Green   -NoNewline
+    Write-Host "Functions for this module will run faster are simplified!"                              -ForegroundColor Yellow  -NoNewline
+    Write-Host "        <"                                                                              -ForegroundColor Green
+    Write-Host "             --------------------------------------------------------------------"      -ForegroundColor Green
+
+    Write-Host " "
+}
