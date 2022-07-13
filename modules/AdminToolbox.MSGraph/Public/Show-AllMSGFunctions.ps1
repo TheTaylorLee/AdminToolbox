af7f9664c4511fab65897201e452eed155136db7
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-07-13 13:53:02 -0500

    func: show-allmsgfunctions update

--- modules/AdminToolbox.MSGraph/Public/Show-AllMSGFunctions.ps1
+++ modules/AdminToolbox.MSGraph/Public/Show-AllMSGFunctions.ps1
@@ -1,6 +1,6 @@
 <#
     .DESCRIPTION
-    Gets all available Microsoft Graph Functions that are available with currently installed MSGraph module versions.
+    Gets all available Microsoft Graph Functions that are available with currently installed MSGraph module versions and gives the option to run it if desired.
 
     .Link
     https://github.com/TheTaylorLee/AdminToolbox
@@ -8,8 +8,13 @@
 
 function Show-AllMSGFunctions {
 
+    [CmdletBinding()]
+    Param (
+    )
+
     #Confirm pre-requisites are met.
     Test-MSGraphRequirements
 
-    Get-Command -Module microsoft.graph*
+    $runit = Get-Command -Module microsoft.graph* | Out-GridView -PassThru -Title "Select a function to run and hit ok if desired."
+    Invoke-Expression ($runit).name
 }
