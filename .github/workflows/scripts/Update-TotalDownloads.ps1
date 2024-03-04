b14dc53c08fd5cce0e499e4ae4bc1e8460902446
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2024-03-04 13:57:10 -0600

    ci: update-totaldownloads

---
+++ .github/workflows/scripts/Update-TotalDownloads.ps1
@@ -0,0 +1,24 @@
+# Must be run using powershell 5.1 on a windows system that supports .net methods.
+# Can this be done with a github workflow?
+
+
+Function Get-TotalDownloads {
+    <#
+     .Description
+     Gets a count of module installs from Powershell Gallery                                                                                                                                                                                    #>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    [CmdletBinding()]                                                                                                                                                                                                                          [Alias('gec')]
+    param (
+    )
+
+    $WebResponse = Invoke-WebRequest "https://www.powershellgallery.com/profiles/TaylorLee"
+    $GetTag = $WebResponse.AllElements | Where-Object { $_.TagName -eq "h2" }
+    $FindText = $GetTag | Where-Object { $_.outertext -like "*total*" }
+    $Write = $FindText.outertext -replace ("Total downloads of packages", '')
+    $total = $write.trim()
+    $date = Get-Date -Format yyyy-MM-dd
+    Write-Output "Total downloads of packages: $total as of $date"
+}
+
+$old = [string](Get-Content .\readme.md | Select-String "total downloads")
+$new = get-totaldownloads
+$content = [System.IO.File]::ReadAllText(".\readme.md").Replace("$old", "$new")
+[System.IO.File]::WriteAllText(".\readme.md", $content)
